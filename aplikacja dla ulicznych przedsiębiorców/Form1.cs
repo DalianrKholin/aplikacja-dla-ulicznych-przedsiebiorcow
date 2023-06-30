1158aec1541de387b54f5bd0c82e9568cecdfc23
DalinarKholin <Osadowskikacper@gmail.com>
2023-06-30 21:21:28 +0200

    Dodaj pliki projektów.

---
+++ aplikacja dla ulicznych przedsiębiorców/Form1.cs
@@ -0,0 +1,10 @@
+namespace aplikacja_dla_ulicznych_przedsiębiorców
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
